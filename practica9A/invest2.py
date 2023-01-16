91573f928ccac74b9d5a52f2f0af7e11c79a9655
boou <boouye22@jaumebalmes.net>
2023-01-16 20:11:06 +0100

    Practica 9 parte A

---
+++ practica9A/invest2.py
@@ -0,0 +1,8 @@
+savings =100
+increase =1.1
+resultat=savings+increase*7
+print(resultat)
+
+'''Imprimir el total dels diners acumulats 
+després de 7 anys, utilitzant variables (comentari a afegir al codi)'''
+
