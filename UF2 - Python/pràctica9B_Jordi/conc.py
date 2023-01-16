5f0836e9caafaeee321852da8f3cf5171d71771e
Jordi <jmanya22@jaumebalmes.net>
2023-01-16 20:17:27 +0100

    UF2

---
+++ UF2 - Python/pràctica9B_Jordi/conc.py
@@ -0,0 +1,9 @@
+#Imprimir el resultat de la suma de les dues variables (int)
+a = 12
+b = 654
+print(a+b)
+
+#Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)
+c = "Visca "
+d = "Terra Lliure"
+print(c+d)
